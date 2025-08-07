a5777ba56cc1b118fe00afb46e393e8e2a757a11
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-07 19:34:10 +0300

    🆕 Добавил файл logging.ts для логирования сообщений с эмодзи

---
+++ src/shared/logging.ts
@@ -0,0 +1,7 @@
+export const logInfo = (msg: string): void => console.log(`ℹ️ ${msg}`);
+
+export const logSuccess = (msg: string): void => console.log(`✅ ${msg}`);
+
+export const logError = (msg: string): void => console.error(`❌ ${msg}`);
+
+export const logWarn = (msg: string): void => console.warn(`⚠️ ${msg}`);
