f193d52d66430e7fd060abfbd9eefb36288e4311
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-08 11:19:04 +0300

    🆕 Добавил типы для поиска сообщений: интерфейсы для индексации и результатов поиска

---
+++ src/features/search/search.types.ts
@@ -0,0 +1,30 @@
+export interface IndexedMessage {
+  chat_id: string;
+  message_id: number;
+  from_id?: string;
+  date: string;
+  text?: string;
+  entities?: unknown[];
+  attachments?: unknown[];
+  lang?: string;
+  chat_type?: string;
+}
+
+export interface SearchHitDoc extends IndexedMessage {}
+
+export interface SearchHit {
+  id: string;
+  score?: number;
+  doc: SearchHitDoc;
+}
+
+export interface SearchParams {
+  chatId: bigint | number | string;
+  query: string;
+  limit?: number;
+}
+
+export interface SearchResult {
+  total: number;
+  hits: SearchHit[];
+}
