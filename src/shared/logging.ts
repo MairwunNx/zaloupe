661b2547143990146266208fd07c5c6af635db9f
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-09 01:57:06 +0300

    🔧 Улучшил логирование: добавил возможность вывода ошибки при логировании ошибок

--- src/shared/logging.ts
+++ src/shared/logging.ts
@@ -2,6 +2,12 @@ export const logInfo = (msg: string): void => console.log(`ℹ️ ${msg}`);
 
 export const logSuccess = (msg: string): void => console.log(`✅ ${msg}`);
 
-export const logError = (msg: string): void => console.error(`❌ ${msg}`);
+export const logError = (msg: string, e?: any): void => {
+  if (e) {
+    console.error(`❌ ${msg}`, e);
+  } else {
+    console.error(`❌ ${msg}`);
+  }
+};
 
 export const logWarn = (msg: string): void => console.warn(`⚠️ ${msg}`);
