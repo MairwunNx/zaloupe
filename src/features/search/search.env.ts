01fd21aa953f6ca0b5cdcc1d1689bd4595ba6294
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-09 01:55:49 +0300

    🔧 Изменил адрес Elasticsearch на 127.0.0.1 в конфигурации окружения

--- src/features/search/search.env.ts
+++ src/features/search/search.env.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const ELASTIC_URL = process.env.ELASTIC_URL ?? "http://localhost:9200";
+export const ELASTIC_URL = process.env.ELASTIC_URL ?? "http://127.0.0.1:9200";
 export const ELASTIC_USERNAME = process.env.ELASTIC_USERNAME ?? undefined;
 export const ELASTIC_PASSWORD = process.env.ELASTIC_PASSWORD ?? undefined;
 export const ELASTIC_INDEX = process.env.ELASTIC_INDEX ?? "messages";
