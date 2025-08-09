19722aaa0b2f013b557baaeafb18f5f652ad1aec
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-09 19:36:32 +0300

    🔧 Добавил параметр maxRetriesPerRequest в конфигурацию подключения к Redis

--- src/queue/redis.ts
+++ src/queue/redis.ts
@@ -5,6 +5,6 @@ let singleton: Redis | null = null;
 export function getRedis(): Redis {
   if (singleton) return singleton;
   const url = process.env.REDIS_URL || "redis://redis:6379";
-  singleton = new Redis(url, { lazyConnect: true });
+  singleton = new Redis(url, { lazyConnect: true, maxRetriesPerRequest: null });
   return singleton;
 }
